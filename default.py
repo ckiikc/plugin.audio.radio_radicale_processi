e1f6572ca7e5d14a34ad5224277092cabd214d66
Giancarlo Abruzzese <gc.abruzzese@gmail.com>
2016-10-19 21:51:40 +0200

    small different for parser

--- default.py
+++ default.py
@@ -28,7 +28,8 @@ def get_page(url):
 
 def get_chunks_file(url):
     html = get_page(url)
-    p=re.findall('(http://audio\-aac\.radioradicale\.it:1935/aac\-1/_definst_/[0-9]{4}/[0-9]{2}/[0-9]{2}/.+\.m4a/)playlist\.m3u8',html)
+    #https://video.radioradicale.it/aac-1/_definst_/2016/10/18/MP857128.m4a/playlist.m3u8
+    p=re.findall('(https://video\.radioradicale\.it/aac\-1/_definst_/[0-9]{4}/[0-9]{2}/[0-9]{2}/.+\.m4a/)playlist\.m3u8',html)
     tracker_base_url=p[0]
     tracker_file_txt = get_page(tracker_base_url + 'playlist.m3u8')
     tracker=re.findall('chunklist.*\.m3u8',tracker_file_txt)
@@ -59,6 +60,7 @@ def build_url(query):
     
 def build_udienze_list(udienze):
     udienze_list = []
+    #subtitle = os.path.join(subtitle, 'subtitle.srt')
     # iterate over the contents of the dictionary udienze to build the list
     for udienza in udienze:
         # create a list item using the udienza filename for the label
@@ -68,6 +70,9 @@ def build_udienze_list(udienze):
         #li.setProperty('fanart_image', udienze[udienza]['album_cover'])
         # set the list item to playable
         li.setProperty('IsPlayable', 'true')
+        li.setSubtitles(['special://temp/subtitle.srt'])
+        #li.setProperty('upnp:subtitle:1', 'https://www.radioradicale.it/trascrizioni/8/3/2/3/7/832372.vtt')
+        #li.setSubtitles(('https://www.radioradicale.it/trascrizioni/8/3/2/3/7/832372.vtt',))
         url = build_url({'mode': 'stream', 'url': udienze[udienza]['url'], 'title': udienze[udienza]['title'].encode('utf-8').strip()})
         # add the current list item to a list
         udienze_list.append((url, li, False))
@@ -96,6 +101,8 @@ def play_song(url):
     play_item = xbmcgui.ListItem(path=chunks_url)
     # the list item is ready to be played by Kodi
     xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
+    #xbmc.Player().play(play_item)
+    #xbmc.Player().setSubtitles('https://www.radioradicale.it/trascrizioni/8/3/2/3/7/832372.vtt')
     
 def main():
     args = urlparse.parse_qs(sys.argv[2][1:])
